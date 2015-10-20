2c7eeb2b1aa698036a6f271e3680ce6a25d91979
Noémien Kocher <nkcr.je@gmail.com>
2015-10-20 10:46:55 +0000

    Adds my local examples

---
+++ simple-epiphany/hello-from-epiphany/main.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <e-hal.h>  // Epiphany Hardware Abstraction Layer
+                    // functionality for communicating with epiphany chip when
+                    // the application runs on a host, typically the ARM µp
+
+#define BUFOFFSET (0x01000000)  // SDRAM is at 0x8f00'0000,
+                                // offset in e_read starts at 0x8e00'0000
+
+unsigned rows, cols, i, j, ncores;
+
+/*
+ * Init the epiphany platform
+ */
+void init_epiphany(e_platform_t * platform) {
+  e_init(NULL);
+  e_reset_system();
+  e_get_platform_info(platform);
+}
+
+/*
+ * Create the workgroup and load programs into it
+ */
+init_workgroup(e_epiphany_t * dev) {
+  e_open(dev, 0, 0, rows, cols); // Create an epiphany cores workgroup
+  // load programs into cores workgroup, do not execute it immediately
+  e_load_group("emain.srec", dev, 0, 0, rows, cols, E_FALSE);
+  e_start_group(dev);
+}
+
+/*
+ * Main entry
+ */
+int main(int argc, char * argv[]) {
+  e_platform_t platform;  // platform infos
+  e_epiphany_t dev;       // provides access to cores workgroup
+  e_mem_t emem;           // shared memory buffer
+
+  init_epiphany(&platform);
+
+  rows = platform.rows;
+  cols = platform.cols;
+  ncores = rows * cols;
+  int result[ncores];     // to store the results, size of cores
+
+  // allocate a space to share data between e_cores and here
+  // offset starts from 0x8e00'0000
+  // sdram (share space) is at 0x8f00'0000
+  // so 0x8e00'0000 + 0x0100'0000 = 0x8f00'0000
+  e_alloc(&emem, BUFOFFSET, 4*ncores);
+
+  init_workgroup(&dev);
+  // we read from the allocated space and store it to result array
+  e_read(&emem, 0, 0, 0x0, &result, ncores * sizeof(int)); // reads what's ben put in buffer
+
+  for(i = 0; i < ncores; i++)
+    printf("The result is 0x%04x\n", result[i]);
+
+  return 0;
+}
---
+++ simple-epiphany/hello-from-neighbor-ecore/emain.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <time.h>
+
+#include <e-lib.h> // Epiphany cores library
+
+#define READY 0x1
+char swap[8] SECTION(".text_bank2"); // 32 words
+volatile unsigned *result;
+
+int main(void) {
+  int i = 0;
+
+  unsigned core_row, core_col,
+           group_rows, group_cols,
+           delay, core_num, core_id;
+
+  core_row = e_group_config.core_row;
+  core_col = e_group_config.core_col;
+  group_rows = e_group_config.group_rows;
+  group_cols = e_group_config.group_cols;
+
+  core_id = e_get_coreid();
+  core_num = core_row * group_cols + core_col;
+
+  swap[0] = core_num;
+
+  unsigned neighbor_row, neighbor_col;
+  e_neighbor_id(E_NEXT_CORE, E_GROUP_WRAP, &neighbor_row, &neighbor_col);
+  unsigned neighbor_num = neighbor_row * group_cols + neighbor_col;
+  unsigned neighbor_id = e_coreid_from_coords(neighbor_row, neighbor_col);
+
+  swap[1] = READY;
+  char neighbor_status;
+  do {
+    e_read(&e_group_config,&neighbor_status,neighbor_row,neighbor_col,(char*)0x4001,1);
+  } while(neighbor_status != READY);
+  e_read(&e_group_config,&neighbor_status,neighbor_row,neighbor_col,(char*)0x4000,1);
+
+  result  = (volatile unsigned *) (0x8f000000 + 0x4*core_num); // writing to external memory, writing 4bytes
+  *result = neighbor_status;
+}
