f676f2b7d61f6263eb0fb70f2ae4271f20699062
Noémien Kocher <nkcr.je@gmail.com>
2015-10-20 14:59:17 +0000

    completed game of life

---
+++ simple-epiphany/game-of-life/README.md
@@ -0,0 +1,28 @@
+Each core saves it's id in it's local memory. Then it writes a ready status, waits for it's neighbor to be ready, then read it's
+neighbor id. Finally writes the result in shared memory. The host then prints the result.
+
+to run :
+```
+./build.sh && ./main.elf
+```
+
+expected outcome :
+
+```
+The result is 0x0001
+The result is 0x0002
+The result is 0x0003
+The result is 0x0004
+The result is 0x0005
+The result is 0x0006
+The result is 0x0007
+The result is 0x0008
+The result is 0x0009
+The result is 0x000a
+The result is 0x000b
+The result is 0x000c
+The result is 0x000d
+The result is 0x000e
+The result is 0x000f
+The result is 0x0000
+```
---
+++ simple-epiphany/game-of-life/main.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <e-hal.h>  // Epiphany Hardware Abstraction Layer
+                    // functionality for communicating with epiphany chip when
+                    // the application runs on a host, typically the ARM µp
+
+#define BUFOFFSET (0x01000000)  // SDRAM is at 0x8f00'0000,
+                                // offset in e_read starts at 0x8e00'0000
+#define GAME_ITERATIONS 10000
+
+unsigned rows, cols, i, j, ncores, row, col;
+
+/*
+ * Init the epiphany platform
+ */
+void init_epiphany(e_platform_t * platform) {
+  e_init(NULL);
+  e_reset_system();
+  e_get_platform_info(platform);
+}
+
+/*
+ * Create the workgroup and load programs into it
+ */
+init_workgroup(e_epiphany_t * dev) {
+  e_return_stat_t result;
+  e_open(dev, 0, 0, rows, cols); // Create an epiphany cores workgroup
+  e_reset_group(dev);
+  // load programs into cores workgroup, do not execute it immediately
+  result = e_load_group("emain.srec", dev, 0, 0, rows, cols, E_FALSE);
+  if(result != E_OK) {
+    printf("Error Loading the Epiphany Application %i\n", result);
+  }
+  e_start_group(dev);
+}
+
+/*
+ * Main entry
+ */
+int main(int argc, char * argv[]) {
+  e_platform_t platform;  // platform infos
+  e_epiphany_t dev;       // provides access to cores workgroup
+  e_mem_t emem;           // shared memory buffer
+
+  init_epiphany(&platform);
+
+  rows = platform.rows;
+  cols = platform.cols;
+  ncores = rows * cols;
+  char result[ncores];     // to store the results, size of cores
+  // allocate a space to share data between e_cores and here
+  // offset starts from 0x8e00'0000
+  // sdram (shared space) is at 0x8f00'0000
+  // so 0x8e00'0000 + 0x0100'0000 = 0x8f00'0000
+  e_alloc(&emem, BUFOFFSET, 4*ncores);
+
+  init_workgroup(&dev);
+  // we read from the allocated space and store it to the result array
+  for(i = 0; i < GAME_ITERATIONS; i++) {
+    usleep(1000);
+    e_read(&emem, 0, 0, 0x0, &result, ncores * sizeof(char)); // reads what's ben put in buffer
+    fprintf(stdout, "X\tX\tX\tX\tX\tX\n");
+    for(row = 0; row < rows; row++) {
+      fprintf(stdout, "X\t");
+      for(col = 0; col < cols; col++) {
+        fprintf(stdout, "%c\t", result[row*cols+col]);
+      }
+      fprintf(stdout, "X\t");
+      fprintf(stdout, "\n");
+    }
+    fprintf(stdout, "X\tX\tX\tX\tX\tX\n");
+    fprintf(stdout, "\n");
+    fflush(stdout);
+  }
+  e_close(&dev);
+  return 0;
+}
