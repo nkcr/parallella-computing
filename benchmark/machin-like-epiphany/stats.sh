aa1397f273918bd648ca3b37d8396ad229e60d0f
Noémien Kocher <nkcr.je@gmail.com>
2016-01-20 13:04:01 +0000

    Machin like implementations and stats

--- benchmark/machin-like-epiphany/stats.sh
+++ benchmark/machin-like-epiphany/stats.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-for ((i=0;i<=22;i++));
+# take as param the number of iterations
+if [ $# -eq 0 ]
+  then
+    echo "Usage ./stats_chunck.sh <iteration number>"
+    exit 0
+fi
+
+IT=$1
+
+for ((i=0;i<=$IT;i++));
 do
-  for ((j=0;j<=22;j++));
+  for ((j=0;j<=$IT;j++));
   do
-    echo "i = $(($i*$i*$i)) j = $(($j*$j*$j))"
+    echo "($(($i*$i*$i)),$(($j*$j*$j)))"
     ./main.elf $(($i*$i*$i)) $(($j*$j*$j)) >> stats.csv
   done
+  echo "" >> stats.csv
 done
